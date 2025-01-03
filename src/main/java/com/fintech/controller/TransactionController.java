1c65e97c6e587957ccf2fd552ac30572e5a8209e
Sunkanmi <sunkanmi@credocentral.com>
2025-01-03 19:27:55 +0100

    Added user and transaction controller

---
+++ src/main/java/com/fintech/controller/TransactionController.java
@@ -0,0 +1,4 @@
+package com.fintech.controller;
+
+public class TransactionController {
+}
---
+++ src/main/java/com/fintech/controller/UserAccountController.java
@@ -0,0 +1,16 @@
+package com.fintech.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class UserAccountController {
+
+
+}
